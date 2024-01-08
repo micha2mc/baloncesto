bd4460213f979fac21c32ff9d5252fa04d8c0063
micha <micha2mc@gmail.com>
2024-01-08 13:47:40 +0100

    primera prueba con el selenium con click en los botones

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -19,6 +21,28 @@ class PruebasPhantomjsIT {
         driver.navigate().to("http://localhost:8080/Baloncesto/");
         assertEquals("Votacion mejor jugador liga ACB", driver.getTitle(), "El titulo no es correcto");
         System.out.println(driver.getTitle());
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    void botonIndexTest() {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--web-security=no", "--ignore-ssl-errors=yes"});
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+        //Localizo el boton "poner votos a cero" y es pulsado
+        WebElement botonVotosCero = driver.findElement(By.name("B3"));
+        botonVotosCero.click();
+        System.out.println("Votos reseteatos");
+
+        //Localizo el boton "ver votos" y es pulsado
+        WebElement botonVerVotos = driver.findElement(By.name("B4"));
+        botonVerVotos.click();
+        System.out.println("Boton ver todos clickeado");
         driver.close();
         driver.quit();
     }
