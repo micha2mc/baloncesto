de82becf2537ce4c695b9f5dd6154e9011cbf86f
micha <micha2mc@gmail.com>
2024-01-08 14:26:18 +0100

    localizacion de la tabla y lectura de valores

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -6,10 +6,14 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PruebasPhantomjsIT {
     private static WebDriver driver = null;
+
+    By registerPageLocator = By.xpath("//table[@aria-describedby='jugadores']");
 
     @Test
     void tituloIndexTest() {
@@ -34,15 +38,22 @@ class PruebasPhantomjsIT {
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{"--web-security=no", "--ignore-ssl-errors=yes"});
         driver = new PhantomJSDriver(caps);
         driver.navigate().to("http://localhost:8080/Baloncesto/");
-        //Localizo el boton "poner votos a cero" y es pulsado
+        //1.- Localizo el boton "poner votos a cero" y es pulsado
         WebElement botonVotosCero = driver.findElement(By.name("B3"));
         botonVotosCero.click();
         System.out.println("Votos reseteatos");
 
-        //Localizo el boton "ver votos" y es pulsado
+        //2.- Localizo el boton "ver votos" y es pulsado
         WebElement botonVerVotos = driver.findElement(By.name("B4"));
         botonVerVotos.click();
         System.out.println("Boton ver todos clickeado");
+
+        //3.- Localiza la nueva página
+        if(driver.findElement(registerPageLocator).isDisplayed()){
+            List<WebElement> listaFilas = driver.findElements(By.className("filas"));
+            System.out.println(listaFilas);
+        }
+
         driver.close();
         driver.quit();
     }
